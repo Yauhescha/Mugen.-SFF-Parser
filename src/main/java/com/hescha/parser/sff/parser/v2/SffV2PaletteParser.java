fde04676abdba23df5e5dd14e21935be4dea574b
Yauhescha <evgescha.maslov007@yandex.ru>
2022-11-22 22:59:36 +0300

    Add palette data cleaning

--- src/main/java/com/hescha/parser/sff/parser/v2/SffV2PaletteParser.java
+++ src/main/java/com/hescha/parser/sff/parser/v2/SffV2PaletteParser.java
@@ -73,9 +73,24 @@ public class SffV2PaletteParser extends SffHeaderParser {
             accessFile.seek(lDataOffset + palette.getOffsetIntoData());
             byte[] paletteData = new byte[palette.getDataLength()];
             accessFile.read(paletteData);
-            palette.setData(paletteData);
+            byte[] clearedData = clearData(paletteData);
+            palette.setData(clearedData);
         } else {
             palette.setData(palettes.get(palette.getLinkedPalette()).getData());
         }
+    }
+
+    // Palette in SFFv1 has 3 byte, in SFFv2 has 4 byte, and each fourth byte is junk
+    private byte[] clearData(byte[] paletteData) {
+        byte[] result = new byte[paletteData.length * 3 / 4];
+        int colorIndex = 0;
+        for (int i = 0; i < paletteData.length; i++) {
+            if ((i+1) % 4 != 0) {
+                result[colorIndex] = paletteData[i];
+                colorIndex++;
+            }
+        }
+
+        return result;
     }
 }
